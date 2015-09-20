7d49c8e7d991706f8be36c0c2e953862689f5524
Clément Vuchener <clement.vuchener@gmail.com>
2015-09-20 12:21:04 +0200

    Add scan tool

---
+++ scan-read-command.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+hidraw=$1
+
+for i in {0..255}; do
+	for type in short long; do
+		data=$(src/logitech-raw-command "$hidraw" $i read $type 2> /dev/null)
+		error=$?
+		case $error in
+		0)
+			printf "Command 0x%02x: $type\n" $i
+			echo $data
+			;;
+
+		2)
+			;;
+
+		10)
+			printf "Command 0x%02x: $type - unavailable\n" $i
+			;;
+
+		11)
+			printf "Command 0x%02x: $type - invalid params\n" $i
+			;;
+		
+		*)
+			printf "Error: %s\n" $error
+			exit -1
+		esac
+	done
+done
